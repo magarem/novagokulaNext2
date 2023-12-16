50aa8abf701adb9be8b701034d1f3a4ce3127814
marcelo maga <magawebtec@gmail.com>
2023-12-16 10:53:22 -0300

    new fech client aproach

--- pages/[id].vue
+++ pages/[id].vue
@@ -8,8 +8,11 @@
 //   import * as matter from 'gray-matter';
   const route = useRoute()
   console.log(route.params.id) // { id: '123' }
-  const { data: ret } = await useFetch('/api/le?id='+route.params.id)
+  // const { data: ret } = await useFetch('/api/le?id='+route.params.id)
+  const { data: ret } = await useFetch('https://novagokula.com.br/teste/' + route.params.id + '.md')
+  
   console.log(ret.value);
+
 //   const { data: ret2 } = await useAsyncData('home', () => queryContent(ret).findOne())
 
 //   console.log(matter(ret.value));
