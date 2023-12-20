867c6bd0c20ac878c90d8d91ce11e5cbba63576f
marcelo maga <magawebtec@gmail.com>
2023-12-20 13:00:50 -0300

    websocket error fixing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  content: {
+    watch: false,
+  },
   css: ['~/assets/css/bootstrap.scss'],
   modules: ["formidable", "@nuxt/image", "@nuxtjs/google-fonts", '@nuxt/content', '@vueuse/nuxt'],
   plugins: [
